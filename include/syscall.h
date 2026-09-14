95f172b52c4e71573135490ac438ba8b8cb99f5a
AngmarX <ehsan.afkari89@gmail.com>
2026-09-14 11:06:36 +0330

    Centralize syscall ABI definitions

--- include/syscall.h
+++ include/syscall.h
@@ -2,22 +2,23 @@
 #define LIONOS_SYSCALL_H
 
 #include <stdint.h>
+#include "uapi.h"
 
-#define SYS_ABI_VERSION 0u
-#define SYS_PUTC        1u
-#define SYS_GETPID      2u
-#define SYS_YIELD       3u
-#define SYS_EXIT        4u
-#define SYS_GETCHAR     5u
-#define SYS_KBD_AVAIL   6u
-#define SYS_WRITE       7u
-#define SYS_READ        8u
-#define SYS_CLEAR       9u
-#define SYS_MEMINFO     10u
-#define SYS_EXEC        11u
+#define SYS_ABI_VERSION LIONOS_SYSCALL_ABI
+#define SYS_PUTC        LIONOS_SYS_PUTC
+#define SYS_GETPID      LIONOS_SYS_GETPID
+#define SYS_YIELD       LIONOS_SYS_YIELD
+#define SYS_EXIT        LIONOS_SYS_EXIT
+#define SYS_GETCHAR     LIONOS_SYS_GETCHAR
+#define SYS_KBD_AVAIL   LIONOS_SYS_KBD_AVAIL
+#define SYS_WRITE       LIONOS_SYS_WRITE
+#define SYS_READ        LIONOS_SYS_READ
+#define SYS_CLEAR       LIONOS_SYS_CLEAR
+#define SYS_MEMINFO     LIONOS_SYS_MEMINFO
+#define SYS_EXEC        LIONOS_SYS_EXEC
 
-#define SYSCALL_OK 0u
-#define SYSCALL_ERR ((uint32_t)-1)
+#define SYSCALL_OK LIONOS_SYSCALL_OK
+#define SYSCALL_ERR LIONOS_SYSCALL_ERR
 
 uint32_t syscall_handle(uint32_t number, uint32_t arg0, uint32_t arg1, uint32_t arg2);
 void syscall_init(void);
