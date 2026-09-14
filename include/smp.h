a6835b33773abf44535761b5e0843d94f74b818d
AngmarX <ehsan.afkari89@gmail.com>
2026-09-14 17:16:26 +0330

    smp: add SMP controller interface

---
+++ include/smp.h
@@ -0,0 +1,14 @@
+#ifndef LIONOS_SMP_H
+#define LIONOS_SMP_H
+
+#include <stdint.h>
+
+#define LIONOS_SMP_TRAMPOLINE 0x8000u
+#define LIONOS_SMP_STACK_PAGES 4u
+#define LIONOS_SMP_START_TIMEOUT 10000000u
+
+void smp_init(void);
+uint32_t smp_online_count(void);
+void smp_ap_main(void);
+
+#endif
