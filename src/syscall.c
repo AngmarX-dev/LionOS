a8730a97349e61fa285dca0d935c1faaf0ee78a1
AngmarX <ehsan.afkari89@gmail.com>
2026-09-14 08:01:47 +0330

    Prepare syscall dispatcher for INT 0x80

--- src/syscall.c
+++ src/syscall.c
@@ -1,15 +1,20 @@
 #include <stdint.h>
-#include "io.h"
 
-void syscall_init(void) {
-    /* INT 0x80 is installed by idt_init(). This function is reserved for the syscall ABI. */
-}
-
-uint32_t syscall_dispatch(uint32_t number, uint32_t arg0, uint32_t arg1, uint32_t arg2) {
-    (void)arg0; (void)arg1; (void)arg2;
+static uint32_t syscall_dispatch(uint32_t number, uint32_t arg0, uint32_t arg1, uint32_t arg2) {
+    (void)arg1;
+    (void)arg2;
     switch (number) {
-        case 0: return 0; /* get kernel ABI version */
-        case 1: return 1; /* future console write */
+        case 0: return 1; /* LionOS syscall ABI version */
+        case 1: return arg0; /* placeholder console-write return value */
         default: return (uint32_t)-1;
     }
+}
+
+void syscall_init(void) {
+    /* INT 0x80 is installed by idt_init(); dispatch occurs in interrupt_dispatch(). */
+    (void)syscall_dispatch;
+}
+
+uint32_t syscall_handle(uint32_t number, uint32_t arg0, uint32_t arg1, uint32_t arg2) {
+    return syscall_dispatch(number, arg0, arg1, arg2);
 }
