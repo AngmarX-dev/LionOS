cc1b7d1c1f47c291ffe287f5d937ea41b19ffc50
AngmarX <ehsan.afkari89@gmail.com>
2026-09-14 14:24:50 +0330

    userland: add ls utility

---
+++ user/bin/ls.c
@@ -0,0 +1,15 @@
+#include "user_libc.h"
+
+int main(void) {
+    char name[64];
+    uint32_t index = 0;
+    while (lion_getfile(index, name, sizeof(name)) >= 0) {
+        struct lion_stat st;
+        if (lion_stat(name, &st) == 0)
+            printf("%-16s %u bytes\n", name, st.size);
+        else
+            puts(name);
+        ++index;
+    }
+    return 0;
+}
