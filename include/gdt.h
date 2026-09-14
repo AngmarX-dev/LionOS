134c515e325e8a1965ba7fd48e136898720353a6
AngmarX <ehsan.afkari89@gmail.com>
2026-09-14 17:27:07 +0330

    arch: expose per-CPU GDT loading API

--- include/gdt.h
+++ include/gdt.h
@@ -5,5 +5,9 @@
 
 void gdt_init(void);
 void gdt_set_tss(uint32_t base, uint32_t limit);
+void gdt_load_cpu(uint32_t cpu, uint32_t tss_base, uint32_t tss_limit);
+uint32_t gdt_user_code_selector(void);
+uint32_t gdt_user_data_selector(void);
+uint32_t gdt_tss_selector(void);
 
 #endif
