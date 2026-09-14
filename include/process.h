1219790dc090d9e439020fe3083df20451459095
AngmarX <ehsan.afkari89@gmail.com>
2026-09-14 12:40:37 +0330

    process: add in-place exec replacement

--- include/process.h
+++ include/process.h
@@ -19,6 +19,7 @@ struct process {
     uint32_t exit_code;
     uint32_t wait_pid;
     uint32_t reap_pending;
+    uint32_t deferred_kernel_stack;
     uint32_t page_directory;
     uint32_t entry;
     uint32_t user_stack;
@@ -43,6 +44,9 @@ struct process *process_create_ex(uint32_t entry, uint32_t user_stack, uint32_t 
 struct process *process_create_ex_vas(uint32_t entry, uint32_t user_stack, uint32_t page_directory,
                                       const uint32_t *user_pages, const uint32_t *user_page_vas,
                                       uint32_t user_page_count);
+int process_exec_replace_current(uint32_t entry, uint32_t user_stack, uint32_t page_directory,
+                                 const uint32_t *user_pages, const uint32_t *user_page_vas,
+                                 uint32_t user_page_count);
 int process_set_current(struct process *process);
 void process_exit_current(uint32_t exit_code);
 uint32_t process_fork_current(uint32_t *parent_frame);
