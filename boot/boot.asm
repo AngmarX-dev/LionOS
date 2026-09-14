2410b4c5873f1e277bd6494025329b5c969fdbe7
AngmarX <ehsan.afkari89@gmail.com>
2026-09-14 07:08:29 +0330

    Add Multiboot2 boot and interrupt stubs

---
+++ boot/boot.asm
@@ -0,0 +1,111 @@
+BITS 32
+
+section .multiboot
+align 8
+multiboot_header:
+    dd 0xE85250D6
+    dd 0
+    dd multiboot_header_end - multiboot_header
+    dd -(0xE85250D6 + 0 + (multiboot_header_end - multiboot_header))
+    dw 0
+    dw 0
+    dd 8
+multiboot_header_end:
+
+section .bss
+align 16
+stack_bottom:
+    resb 16384
+stack_top:
+
+section .text
+global _start
+extern kernel_main
+
+_start:
+    cli
+    mov esp, stack_top
+    push ebx
+    push eax
+    call kernel_main
+.hang:
+    cli
+    hlt
+    jmp .hang
+
+; Common interrupt entry. The C dispatcher receives the vector number.
+global isr_common
+extern interrupt_dispatch
+isr_common:
+    pusha
+    push ds
+    push es
+    push fs
+    push gs
+    mov ax, 0x10
+    mov ds, ax
+    mov es, ax
+    push esp
+    call interrupt_dispatch
+    add esp, 4
+    pop gs
+    pop fs
+    pop es
+    pop ds
+    popa
+    add esp, 8
+    iretd
+
+%macro ISR_NOERR 1
+global isr%1
+isr%1:
+    push dword 0
+    push dword %1
+    jmp isr_common
+%endmacro
+
+%macro ISR_ERR 1
+global isr%1
+isr%1:
+    push dword %1
+    jmp isr_common
+%endmacro
+
+ISR_NOERR 0
+ISR_NOERR 1
+ISR_NOERR 2
+ISR_NOERR 3
+ISR_NOERR 4
+ISR_NOERR 5
+ISR_NOERR 6
+ISR_NOERR 7
+ISR_ERR   8
+ISR_NOERR 9
+ISR_ERR   10
+ISR_ERR   11
+ISR_ERR   12
+ISR_ERR   13
+ISR_ERR   14
+ISR_NOERR 15
+ISR_NOERR 16
+ISR_ERR   17
+ISR_NOERR 18
+ISR_NOERR 19
+ISR_NOERR 20
+ISR_NOERR 21
+ISR_NOERR 22
+ISR_NOERR 23
+ISR_NOERR 24
+ISR_NOERR 25
+ISR_NOERR 26
+ISR_NOERR 27
+ISR_NOERR 28
+ISR_NOERR 29
+ISR_ERR   30
+ISR_NOERR 31
+
+%assign i 32
+%rep 224
+ISR_NOERR i
+a %assign i i+1
+%endrep
