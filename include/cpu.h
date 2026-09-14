ce220866c5ca6677eeedf2c952c1b65392f6d50b
AngmarX <ehsan.afkari89@gmail.com>
2026-09-14 14:39:02 +0330

    smp: add per-cpu topology interface

---
+++ include/cpu.h
@@ -0,0 +1,20 @@
+#ifndef LIONOS_CPU_H
+#define LIONOS_CPU_H
+
+#include <stdint.h>
+
+#define LIONOS_MAX_CPUS 16u
+
+struct cpu_info {
+    uint32_t index;
+    uint32_t apic_id;
+    uint32_t logical_per_package;
+    uint32_t online;
+};
+
+void cpu_init(void);
+uint32_t cpu_count_hint(void);
+uint32_t cpu_current_index(void);
+const struct cpu_info *cpu_get(uint32_t index);
+
+#endif
