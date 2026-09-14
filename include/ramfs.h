e4ea514681f949cee2041f47d00157bd0f28c5aa
AngmarX <ehsan.afkari89@gmail.com>
2026-09-14 10:40:52 +0330

    Expand RAMFS API for shell file management

--- include/ramfs.h
+++ include/ramfs.h
@@ -8,6 +8,8 @@ uint32_t ramfs_count(void);
 const char *ramfs_name(uint32_t index);
 const char *ramfs_data(const char *name);
 uint32_t ramfs_size(const char *name);
+int ramfs_exists(const char *name);
 int ramfs_write(const char *name, const char *data, uint32_t size);
+int ramfs_remove(const char *name);
 
 #endif
