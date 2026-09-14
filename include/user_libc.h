7a2d0cfb01279790181389781e8771f93195bacc
AngmarX <ehsan.afkari89@gmail.com>
2026-09-14 13:14:11 +0330

    Expand userspace libc API

--- include/user_libc.h
+++ include/user_libc.h
@@ -7,7 +7,13 @@
 uint32_t strlen(const char *s);
 void *memset(void *dst, int value, uint32_t length);
 void *memcpy(void *dst, const void *src, uint32_t length);
+void *memmove(void *dst, const void *src, uint32_t length);
+int memcmp(const void *a, const void *b, uint32_t length);
 int strcmp(const char *a, const char *b);
+int strncmp(const char *a, const char *b, uint32_t length);
+char *strchr(const char *s, int c);
+char *strrchr(const char *s, int c);
+int atoi(const char *s);
 void puts(const char *s);
 void putchar(char c);
 
