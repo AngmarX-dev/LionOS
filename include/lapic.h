c9b7bb847b2797323e69c995b0b2ae47af28074c
AngmarX <ehsan.afkari89@gmail.com>
2026-09-14 17:34:28 +0330

    smp: add local APIC timer interface

--- include/lapic.h
+++ include/lapic.h
@@ -5,6 +5,7 @@
 
 #define LIONOS_LAPIC_PHYS 0xFEE00000u
 #define LIONOS_LAPIC_VIRT 0xFEE00000u
+#define LIONOS_LAPIC_TIMER_VECTOR 48u
 
 int lapic_init(void);
 uint32_t lapic_id(void);
@@ -12,5 +13,7 @@ void lapic_eoi(void);
 void lapic_enable(void);
 void lapic_send_init(uint32_t apic_id);
 void lapic_send_startup(uint32_t apic_id, uint32_t vector);
+void lapic_timer_init(void);
+uint32_t lapic_timer_ticks(void);
 
 #endif
