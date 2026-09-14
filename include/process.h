6b658602bef8a2356b69252b447cca8c18537ccc
AngmarX <ehsan.afkari89@gmail.com>
2026-09-14 10:14:41 +0330

    Add process context state for scheduler

--- include/process.h
+++ include/process.h
@@ -8,12 +8,16 @@
 #define PROCESS_READY 1u
 #define PROCESS_RUNNING 2u
 
+#define PROCESS_CONTEXT_WORDS 19u
+
 struct process {
     uint32_t pid;
     uint32_t state;
     uint32_t page_directory;
     uint32_t entry;
     uint32_t user_stack;
+    uint32_t kernel_stack_top;
+    uint32_t saved_frame;
 };
 
 void process_init(void);
@@ -23,5 +27,9 @@ struct process *process_create(uint32_t entry, uint32_t user_stack, uint32_t pag
 int process_set_current(struct process *process);
 void process_exit_current(void);
 uint32_t process_count(void);
+uint32_t *process_schedule(uint32_t *frame);
+void process_set_saved_frame(struct process *process, uint32_t *frame);
+uint32_t *process_saved_frame(struct process *process);
+uint32_t process_kernel_stack_top(struct process *process);
 
 #endif
