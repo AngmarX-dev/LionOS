71c49104f7b6fd1b19c64dde7214aeb52672a5cb
AngmarX <ehsan.afkari89@gmail.com>
2026-09-14 13:37:19 +0330

    Add explicit persistent-file existence query

--- include/diskfs.h
+++ include/diskfs.h
@@ -11,6 +11,7 @@ int diskfs_init(void);
 int diskfs_available(void);
 uint32_t diskfs_count(void);
 const char *diskfs_name(uint32_t index);
+int diskfs_exists(const char *name);
 uint32_t diskfs_size(const char *name);
 int diskfs_read(const char *name, void *buffer, uint32_t capacity);
 int diskfs_write(const char *name, const void *data, uint32_t size);
