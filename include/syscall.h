e65825cc7be879ae1c058dab17461a63e188faa4
AngmarX <ehsan.afkari89@gmail.com>
2026-09-14 10:05:23 +0330

    Add LionOS syscall ABI definitions

---
+++ include/syscall.h
@@ -0,0 +1,18 @@
+#ifndef LIONOS_SYSCALL_H
+#define LIONOS_SYSCALL_H
+
+#include <stdint.h>
+
+#define SYS_ABI_VERSION 0u
+#define SYS_PUTC        1u
+#define SYS_GETPID      2u
+#define SYS_YIELD       3u
+#define SYS_EXIT        4u
+
+#define SYSCALL_OK 0u
+#define SYSCALL_ERR ((uint32_t)-1)
+
+uint32_t syscall_handle(uint32_t number, uint32_t arg0, uint32_t arg1, uint32_t arg2);
+void syscall_init(void);
+
+#endif
