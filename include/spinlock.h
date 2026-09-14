ec71a6f98a842d6cbf7733fe8a953d59ba89eaaf
AngmarX <ehsan.afkari89@gmail.com>
2026-09-14 17:37:04 +0330

    smp: add correct IRQ-save spinlock API

--- include/spinlock.h
+++ include/spinlock.h
@@ -3,13 +3,13 @@
 
 #include <stdint.h>
 
-struct spinlock {
-    volatile uint32_t value;
-};
+struct spinlock { volatile uint32_t value; };
 
 void spinlock_init(struct spinlock *lock);
 void spinlock_acquire(struct spinlock *lock);
 void spinlock_release(struct spinlock *lock);
 uint32_t spinlock_try_acquire(struct spinlock *lock);
+uint32_t spinlock_irqsave_acquire(struct spinlock *lock);
+void spinlock_irqrestore_release(struct spinlock *lock, uint32_t flags);
 
 #endif
