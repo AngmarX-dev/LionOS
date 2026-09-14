78b61a4bfb76aeb266325b4b557ddae1bb3d2771
AngmarX <ehsan.afkari89@gmail.com>
2026-09-14 13:24:53 +0330

    Add persistent LionFS interface

---
+++ include/diskfs.h
@@ -0,0 +1,19 @@
+#ifndef LIONOS_DISKFS_H
+#define LIONOS_DISKFS_H
+
+#include <stdint.h>
+
+#define DISKFS_MAX_FILES 32u
+#define DISKFS_NAME_MAX 24u
+#define DISKFS_MAX_FILE_SIZE 4096u
+
+int diskfs_init(void);
+int diskfs_available(void);
+uint32_t diskfs_count(void);
+const char *diskfs_name(uint32_t index);
+uint32_t diskfs_size(const char *name);
+int diskfs_read(const char *name, void *buffer, uint32_t capacity);
+int diskfs_write(const char *name, const void *data, uint32_t size);
+int diskfs_remove(const char *name);
+
+#endif
