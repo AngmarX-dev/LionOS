1aa2288d51fc4c1c9f8fd8f9082d87588523c04a
AngmarX <ehsan.afkari89@gmail.com>
2026-09-14 11:19:29 +0330

    paging: expose user page lookup for fork

--- include/paging.h
+++ include/paging.h
@@ -8,6 +8,8 @@ uint32_t paging_kernel_directory(void);
 uint32_t paging_create_address_space(void);
 int paging_map_user_page_in(uint32_t page_directory, uint32_t virtual_address,
                             uint32_t physical_address, uint32_t flags);
+int paging_get_user_page(uint32_t page_directory, uint32_t virtual_address,
+                         uint32_t *physical_address, uint32_t *flags);
 void paging_destroy_address_space(uint32_t page_directory);
 void paging_switch_address_space(uint32_t page_directory);
 uint32_t paging_current_address_space(void);
