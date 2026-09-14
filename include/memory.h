968cb9120e13684fa356870bf3fb3df636d8d040
AngmarX <ehsan.afkari89@gmail.com>
2026-09-14 08:01:04 +0330

    Add physical memory manager interface

---
+++ include/memory.h
@@ -0,0 +1,12 @@
+#ifndef LIONOS_MEMORY_H
+#define LIONOS_MEMORY_H
+
+#include <stdint.h>
+
+void memory_init(uint32_t multiboot_info);
+void *page_alloc(void);
+void page_free(void *page);
+uint32_t memory_total_pages(void);
+uint32_t memory_free_pages(void);
+
+#endif
