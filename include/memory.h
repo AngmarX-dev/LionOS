226d4e99d0ab1f7bd3b5ef1c986d5517e89f434d
AngmarX <ehsan.afkari89@gmail.com>
2026-09-14 08:04:38 +0330

    Expose contiguous page allocation for kernel heap

--- include/memory.h
+++ include/memory.h
@@ -5,6 +5,7 @@
 
 void memory_init(uint32_t multiboot_info);
 void *page_alloc(void);
+void *page_alloc_contiguous(uint32_t count);
 void page_free(void *page);
 uint32_t memory_total_pages(void);
 uint32_t memory_free_pages(void);
