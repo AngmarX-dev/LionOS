c683e74a0a9616049e183388234a49de748cbd7f
AngmarX <ehsan.afkari89@gmail.com>
2026-09-14 10:20:38 +0330

    Add per-process address space paging API

--- include/paging.h
+++ include/paging.h
@@ -4,6 +4,11 @@
 #include <stdint.h>
 
 void paging_init(void);
-int paging_map_user_page(uint32_t virtual_address, uint32_t physical_address, uint32_t flags);
+uint32_t paging_kernel_directory(void);
+uint32_t paging_create_address_space(void);
+int paging_map_user_page_in(uint32_t page_directory, uint32_t virtual_address,
+                            uint32_t physical_address, uint32_t flags);
+void paging_switch_address_space(uint32_t page_directory);
+uint32_t paging_current_address_space(void);
 
 #endif
