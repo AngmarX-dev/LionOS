95c428195901b9612b359f681be0c5a119be0535
AngmarX <ehsan.afkari89@gmail.com>
2026-09-14 09:58:45 +0330

    Expose controlled user-page mapping API

--- include/paging.h
+++ include/paging.h
@@ -1,4 +1,9 @@
 #ifndef LIONOS_PAGING_H
 #define LIONOS_PAGING_H
+
+#include <stdint.h>
+
 void paging_init(void);
+int paging_map_user_page(uint32_t virtual_address, uint32_t physical_address, uint32_t flags);
+
 #endif
