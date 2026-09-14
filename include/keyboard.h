715e33f13f4a260001aae0945d0cf57e641a4399
AngmarX <ehsan.afkari89@gmail.com>
2026-09-14 10:30:46 +0330

    Add keyboard input interface

---
+++ include/keyboard.h
@@ -0,0 +1,11 @@
+#ifndef LIONOS_KEYBOARD_H
+#define LIONOS_KEYBOARD_H
+
+#include <stdint.h>
+
+void keyboard_init(void);
+void keyboard_handle_scancode(uint8_t scancode);
+int keyboard_getchar(void);
+uint32_t keyboard_available(void);
+
+#endif
