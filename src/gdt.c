b62e034d6e17a099ca84a648fcf158d6a31aa5da
AngmarX <ehsan.afkari89@gmail.com>
2026-09-14 07:08:52 +0330

    Implement GDT

---
+++ src/gdt.c
@@ -0,0 +1,48 @@
+#include <stdint.h>
+#include "gdt.h"
+
+struct gdt_entry {
+    uint16_t limit_low;
+    uint16_t base_low;
+    uint8_t base_mid;
+    uint8_t access;
+    uint8_t granularity;
+    uint8_t base_high;
+} __attribute__((packed));
+
+struct gdt_ptr {
+    uint16_t limit;
+    uint32_t base;
+} __attribute__((packed));
+
+static struct gdt_entry gdt[3];
+static struct gdt_ptr gp;
+
+static void set_gate(int n, uint32_t base, uint32_t limit, uint8_t access, uint8_t gran) {
+    gdt[n].base_low = base & 0xFFFF;
+    gdt[n].base_mid = (base >> 16) & 0xFF;
+    gdt[n].base_high = (base >> 24) & 0xFF;
+    gdt[n].limit_low = limit & 0xFFFF;
+    gdt[n].granularity = ((limit >> 16) & 0x0F) | (gran & 0xF0);
+    gdt[n].access = access;
+}
+
+void gdt_init(void) {
+    gp.limit = sizeof(gdt) - 1;
+    gp.base = (uint32_t)&gdt;
+
+    set_gate(0, 0, 0, 0, 0);
+    set_gate(1, 0, 0xFFFFFFFF, 0x9A, 0xCF);
+    set_gate(2, 0, 0xFFFFFFFF, 0x92, 0xCF);
+
+    __asm__ volatile ("lgdt %0\n"
+                      "mov $0x10, %%ax\n"
+                      "mov %%ax, %%ds\n"
+                      "mov %%ax, %%es\n"
+                      "mov %%ax, %%fs\n"
+                      "mov %%ax, %%gs\n"
+                      "mov %%ax, %%ss\n"
+                      "ljmp $0x08, $1f\n"
+                      "1:\n"
+                      : : "m"(gp) : "ax", "memory");
+}
