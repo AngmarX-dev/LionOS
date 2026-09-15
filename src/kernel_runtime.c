ee16f60b1df0573044de7160a64835d8dea1bf45
AngmarX <ehsan.afkari89@gmail.com>
2026-09-15 08:37:22 -0500

    refactor: route kernel into GUI idle runtime

---
+++ src/kernel_runtime.c
@@ -0,0 +1,31 @@
+#include "gui.h"
+#include "shell.h"
+
+static void lionos_gui_bootstrap(void);
+static void lionos_gui_or_shell_loop(void);
+
+#define gui_run lionos_gui_bootstrap
+#define shell_run lionos_gui_or_shell_loop
+#include "kernel.c"
+#undef shell_run
+#undef gui_run
+
+static void lionos_gui_bootstrap(void){
+    gui_start();
+}
+
+static void lionos_gui_or_shell_loop(void){
+    if(!gui_is_active()){
+        shell_run();
+        return;
+    }
+
+    for(;;){
+        if(!gui_is_active()){
+            shell_run();
+            return;
+        }
+        gui_step();
+        __asm__ volatile("sti; hlt");
+    }
+}
