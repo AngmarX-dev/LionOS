2414683ed80b46d8c7290ce1c083de9563e7b4cb
AngmarX <ehsan.afkari89@gmail.com>
2026-09-15 04:41:07 -0500

    gui: add framebuffer interface

---
+++ include/framebuffer.h
@@ -0,0 +1,15 @@
+#ifndef LIONOS_FRAMEBUFFER_H
+#define LIONOS_FRAMEBUFFER_H
+
+#include <stdint.h>
+
+int framebuffer_init(uint32_t multiboot_info);
+int framebuffer_available(void);
+uint32_t framebuffer_width(void);
+uint32_t framebuffer_height(void);
+void framebuffer_clear(uint32_t color);
+void framebuffer_fill_rect(uint32_t x, uint32_t y, uint32_t width, uint32_t height, uint32_t color);
+void framebuffer_console_clear(void);
+void framebuffer_console_putc(char c, uint32_t row, uint32_t col, uint32_t fg, uint32_t bg);
+
+#endif
