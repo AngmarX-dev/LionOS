7f677608b833f6f8a87fe0d62879c627438c3a49
AngmarX <ehsan.afkari89@gmail.com>
2026-09-15 03:53:16 -0500

    Instrument AP TSS/GDT initialization

--- src/tss.c
+++ src/tss.c
@@ -2,6 +2,7 @@
 #include "gdt.h"
 #include "tss.h"
 #include "cpu.h"
+#include "debug.h"
 
 struct tss_entry { uint32_t prev_tss; uint32_t esp0; uint32_t ss0; uint32_t esp1; uint32_t ss1; uint32_t esp2; uint32_t ss2; uint32_t cr3; uint32_t eip; uint32_t eflags; uint32_t eax; uint32_t ecx; uint32_t edx; uint32_t ebx; uint32_t esp; uint32_t ebp; uint32_t esi; uint32_t edi; uint32_t es; uint32_t cs; uint32_t ss; uint32_t ds; uint32_t fs; uint32_t gs; uint32_t ldt; uint16_t trap; uint16_t iomap_base; } __attribute__((packed));
 
@@ -15,8 +16,12 @@ void tss_init_cpu(uint32_t cpu,uint32_t stack_top_value){
     t->esp0=stack_top_value;
     t->ss0=0x10u; t->cs=0x08u; t->ss=0x10u; t->ds=0x10u; t->es=0x10u; t->fs=0x10u; t->gs=0x10u;
     t->iomap_base=(uint16_t)sizeof(*t);
+    debug_write("LIONOS:TSS-GDT-BEGIN\n");
     gdt_load_cpu(cpu,(uint32_t)(uintptr_t)t,(uint32_t)sizeof(*t)-1u);
+    debug_write("LIONOS:TSS-GDT-OK\n");
+    debug_write("LIONOS:TSS-LTR-BEGIN\n");
     __asm__ volatile("ltr %%ax" : : "a"((uint16_t)gdt_tss_selector()) : "memory");
+    debug_write("LIONOS:TSS-LTR-OK\n");
 }
 
 void tss_init(void){tss_init_cpu(0u,(uint32_t)(uintptr_t)&stack_top);}
