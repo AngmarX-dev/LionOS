5a0c8793ee059234f2b9353430523d010e3cd7de
AngmarX <ehsan.afkari89@gmail.com>
2026-09-18 02:04:27 -0500

    fix(gui): clean header declarations

--- include/gui.h
+++ include/gui.h
@@ -1,9 +1,11 @@
 #ifndef LIONOS_GUI_H
 #define LIONOS_GUI_H
 
-void gui_run(void);\nvoid gui_desktop_run(void);
+void gui_run(void);
+void gui_desktop_run(void);
 void gui_start(void);
 void gui_step(void);
-int gui_is_active(void);\nvoid gui_run(void);
+int gui_is_active(void);
+void gui_run(void);
 
 #endif
