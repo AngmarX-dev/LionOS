755898f3e810c2b6021745521eb812ee376dcb13
AngmarX <ehsan.afkari89@gmail.com>
2026-09-18 02:06:01 -0500

    fix(framebuffer): expose desktop surface API

--- include/framebuffer.h
+++ include/framebuffer.h
@@ -11,5 +11,7 @@ void framebuffer_clear(uint32_t color);
 void framebuffer_fill_rect(uint32_t x, uint32_t y, uint32_t width, uint32_t height, uint32_t color);
 void framebuffer_console_clear(void);
 void framebuffer_console_putc(char c, uint32_t row, uint32_t col, uint32_t fg, uint32_t bg);
+int framebuffer_begin_desktop(void);
+void framebuffer_present(void);
 
 #endif
