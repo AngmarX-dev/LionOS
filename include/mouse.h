41d4648f858e02545dd7dd794e36eb89d310198d
AngmarX <ehsan.afkari89@gmail.com>
2026-09-18 02:01:25 -0500

    feat(gui): use framebuffer mouse coordinates

--- include/mouse.h
+++ include/mouse.h
@@ -5,9 +5,7 @@
 
 int mouse_init(void);
 void mouse_poll(void);
-void mouse_show(void);
-void mouse_hide(void);
-void mouse_set_cursor_visible(uint32_t visible);
+void mouse_set_bounds(uint32_t width, uint32_t height);
 uint32_t mouse_x(void);
 uint32_t mouse_y(void);
 uint8_t mouse_buttons(void);
